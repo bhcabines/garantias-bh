095f32552a6d178b6cd6d0bea31b1eaddb9b320e
bhcabines <bhcabinessite@gmail.com>
2026-06-19 14:00:22 -0300

    Update usuarios.js

--- usuarios.js
+++ usuarios.js
@@ -18,5 +18,71 @@ window.USUARIOS_CONFIG = [
       "custos"
     ],
     "isAdmin": false
+  },
+  {
+    "id": "u_1781888218571",
+    "nome": "fabiola",
+    "senha": "12345",
+    "projetos": [
+      "garantias",
+      "vendas"
+    ],
+    "isAdmin": false
+  },
+  {
+    "id": "u_1781888229331",
+    "nome": "helvecio",
+    "senha": "helvecio",
+    "projetos": [
+      "garantias",
+      "vendas",
+      "pesquisa",
+      "custos"
+    ],
+    "isAdmin": false
+  },
+  {
+    "id": "u_1781888236291",
+    "nome": "leandro",
+    "senha": "helvecio",
+    "projetos": [
+      "garantias",
+      "vendas",
+      "pesquisa",
+      "custos"
+    ],
+    "isAdmin": false
+  },
+  {
+    "id": "u_1781888356236",
+    "nome": "luisa",
+    "senha": "helvecio",
+    "projetos": [
+      "garantias",
+      "vendas",
+      "pesquisa",
+      "custos"
+    ],
+    "isAdmin": false
+  },
+  {
+    "id": "u_1781888375085",
+    "nome": "newrian",
+    "senha": "12345",
+    "projetos": [
+      "pesquisa",
+      "custos"
+    ],
+    "isAdmin": false
+  },
+  {
+    "id": "u_1781888381596",
+    "nome": "maria",
+    "senha": "12345",
+    "projetos": [
+      "pesquisa",
+      "custos"
+    ],
+    "isAdmin": false
   }
 ];
