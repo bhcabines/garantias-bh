2567f84cd7e93a6076487819496462ed3fced37a
bhcabines <bhcabinessite@gmail.com>
2026-06-19 13:46:17 -0300

    Add files via upload

---
+++ usuarios.js
@@ -0,0 +1,22 @@
+window.USUARIOS_CONFIG = [
+  {
+    "id": "u_1781887371296",
+    "nome": "nathalia",
+    "senha": "12345",
+    "projetos": [
+      "vendas",
+      "custos"
+    ],
+    "isAdmin": false
+  },
+  {
+    "id": "u_1781887542642",
+    "nome": "elenice",
+    "senha": "12345",
+    "projetos": [
+      "vendas",
+      "custos"
+    ],
+    "isAdmin": false
+  }
+];
